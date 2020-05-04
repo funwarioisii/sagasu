95a401a11a7b1c8fa2cec7a0741944da69c0e4ef
kazuyuki-hashimoto <kazuyuki-hashimoto@cookpad.com>
2020-05-04 19:29:29 +0900

    CLIで操作

--- sagasu/app.py
+++ sagasu/app.py
@@ -1,2 +1,20 @@
 import click
+from sagasu.engine import SearchEngine
 
+@click.command()
+def app():
+  word = input(">>> ")
+  engine = SearchEngine()
+  engine.indexing()
+  resources = engine.word_search(word)
+  for resource in resources:
+    print(f"""
+[URI]
+  {resource.uri}
+[Sentence]
+  {resource.sentence[:30]}...
+    """)
+
+
+if __name__ == '__main__':
+    app()
